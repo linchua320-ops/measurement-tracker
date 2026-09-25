62233b06f1b46a6924d83c6b18520b38af112ba5
linchua320-ops <linchua320@gmail.com>
2026-09-25 17:11:21 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,15 @@
-const CACHE = 'meas-tracker-v1';
+const CACHE = 'meas-tracker-v2';
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./index.html', './manifest.json', './apple-touch-icon.png', './icon-192.png', './icon-512.png'])));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./index.html', './manifest.json'])));
   self.skipWaiting();
 });
-self.addEventListener('activate', e => self.clients.claim());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(names => Promise.all(
+      names.filter(n => n !== CACHE).map(n => caches.delete(n))
+    )).then(() => self.clients.claim())
+  );
+});
 self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request).catch(() => caches.match(e.request))
